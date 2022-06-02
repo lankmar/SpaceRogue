cd41bcb226628555e3866713a6f4a85a1f66c768
Alexander Bondarev <Zadir7@mail.ru>
2022-06-03 00:58:56 +0700

    Added update subscription mechansim

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,3 +1,4 @@
+using System;
 using Gameplay.GameState;
 using UnityEngine;
 
@@ -14,9 +15,40 @@ public class EntryPoint : MonoBehaviour
         var gameState = new CurrentState(InitialGameState);
         _mainController = new MainController(gameState, _uiPosition);
     }
-
+    
     private void OnDestroy()
     {
         _mainController.Dispose();
     }
+    
+    
+    #region UpdateMechanism
+
+    private static event Action OnUpdate = () => { };
+    private static event Action OnFixedUpdate = () => { };
+    private static event Action OnLateUpdate = () => { };
+    
+    public static void SubscribeToUpdate(Action callback) => OnUpdate += callback;
+    public static void UnsubscribeFromUpdate(Action callback) => OnUpdate -= callback;
+    public static void SubscribeToFixedUpdate(Action callback) => OnFixedUpdate += callback;
+    public static void UnsubscribeFromFixedUpdate(Action callback) => OnFixedUpdate -= callback;
+    public static void SubscribeToLateUpdate(Action callback) => OnLateUpdate += callback;
+    public static void UnsubscribeFromLateUpdate(Action callback) => OnLateUpdate -= callback;
+    
+    private void Update()
+    {
+        OnUpdate.Invoke();
+    }
+    private void FixedUpdate()
+    {
+        OnFixedUpdate.Invoke();
+    }
+    private void LateUpdate()
+    {
+        OnLateUpdate.Invoke();
+    }
+
+    #endregion
+    
+    
 }
