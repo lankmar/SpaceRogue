ed40d2dbcea684a18e470c2f81bfa7c7c319f703
Alexander Bondarev <Zadir7@mail.ru>
2022-06-04 00:15:17 +0700

    Added scriptables storage, misc

---
+++ Assets/Scripts/Scriptables/PlayerConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = nameof(PlayerConfig), menuName = "Configs/" + nameof(PlayerConfig))]
+    public class PlayerConfig : ScriptableObject
+    {
+        
+    }
+}
