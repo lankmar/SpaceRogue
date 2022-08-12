f498a00bc1a8cc1a9d32a3a0699a7a4c4360fed9
OlegBakulin <oleg_vampiriys@mail.ru>
2022-08-12 23:46:43 +0300

    МиниЧистка

--- Assets/Scripts/UI/Abstracts/DestroyPlayerBottonView.cs
+++ Assets/Scripts/UI/Abstracts/DestroyPlayerBottonView.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DestroyPlayerBottonView : MonoBehaviour
 {
-    public GameObject gameObject;
     public void BackToMenu()
     {
-        gameObject.GetComponent<Text>().name = "QUTI!";
+ 
     }
 }
