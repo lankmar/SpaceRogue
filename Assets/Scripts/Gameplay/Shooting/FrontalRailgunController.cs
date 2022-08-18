a43fa8606a825cf2df80164a4bcf8200a11b1b42
alexfcmp <103672411+alexfcmp@users.noreply.github.com>
2022-08-18 20:15:34 +0300

    Config rename

--- Assets/Scripts/Gameplay/Shooting/FrontalRailgunController.cs
+++ Assets/Scripts/Gameplay/Shooting/FrontalRailgunController.cs
@@ -5,8 +5,17 @@ namespace Gameplay.Shooting
 {
     public class FrontalRailgunController : FrontalTurretController
     {
+        private readonly RailgunWeaponConfig _config;
+
         public FrontalRailgunController(TurretModuleConfig config, Transform gunPointParentTransform) : base(config, gunPointParentTransform)
         {
+            var railgunConfig = config.SpecificWeapon as RailgunWeaponConfig;
+            if (railgunConfig is null)
+            {
+                throw new System.Exception("wrong config type was provided");
+            }
+            _config = railgunConfig;
+
             EntryPoint.SubscribeToUpdate(CoolDown);
         }
         
