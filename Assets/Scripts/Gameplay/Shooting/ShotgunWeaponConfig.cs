7e96e49689d99259aa3661b02acd50d84dee95f8
alexfcmp <alexf.cmp@gmail.com>
2022-09-05 19:01:02 +0300

    weapon fix

--- Assets/Scripts/Gameplay/Shooting/ShotgunWeaponConfig.cs
+++ Assets/Scripts/Gameplay/Shooting/ShotgunWeaponConfig.cs
@@ -7,7 +7,7 @@ namespace Gameplay.Shooting
     public class ShotgunWeaponConfig : SpecificWeaponConfig
     {
         [field: SerializeField] public ProjectileConfig ShotgunProjectile { get; private set; }
-        [field: SerializeField, Min(2)] public int PelletAmount { get; private set; } = 3;
+        [field: SerializeField, Min(1)] public int PelletAmount { get; private set; } = 3;
         [field: SerializeField, Range(0, 180)] public int SprayAngle { get; private set; } = 20;
     }
 }
